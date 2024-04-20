7ad7c1a12b339b2ddda2410fd9a5c76841029ace
sacaarjain <sacaarjain@outlook.com>
2024-04-20 11:15:20 -0700

    Location Services and Map working

--- TrailQuest/Quests.js
+++ TrailQuest/Quests.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 function QuestsScreen() {
+
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Quests</Text>
-      {/* Rest of your code */}
+      {location && (
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          region={location}
+        />
+      )}
     </View>
   );
 }
@@ -19,6 +44,10 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: 'bold',
+  },
+  map: {
+    width: '90%',
+    height: '50%',
   },
 });
 
