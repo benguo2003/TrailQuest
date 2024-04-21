d862876a9e74c2f0fc41265bf2dd0f0d16e348a9
Ben Guo <benguo@Bens-MacBook-Pro.local>
2024-04-20 18:51:27 -0700

    sign in and sign up changes

--- TrailQuest/components/signup.js
+++ TrailQuest/components/signup.js
@@ -1,36 +1,56 @@
 import React from 'react';
-import { View, TextInput, Button, Image, StyleSheet } from 'react-native';
+import { View, TextInput, Image, StyleSheet, TouchableOpacity, Text, KeyboardAvoidingView, Platform} from 'react-native';
 import AwesomeButton from "react-native-really-awesome-button";
+import { useFonts, RobotoSlab_600SemiBold } from '@expo-google-fonts/roboto-slab';
 
-export default function SignIn({ navigation }) {
+export default function SignUp({ navigation }) {
+  let [fontsLoaded, fontError] = useFonts({
+    RobotoSlab_600SemiBold,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <View style={styles.logoContainer}>
-        <Image source={require('../assets/trailQuestLogoNoBG.png')} style={styles.logoImage} />
-      </View>      
-      <View style={styles.formContainer}>
-        <TextInput placeholder="Name" style={styles.input} />
-        <TextInput placeholder="Username" secureTextEntry style={styles.input} />
-        <TextInput placeholder="password" secureTextEntry style={styles.input} />
+    <KeyboardAvoidingView 
+      style={styles.container} 
+      behavior={Platform.OS === "ios" ? "padding" : "height"} 
+      enabled
+    >
+      <View style={styles.container}>
+        <View style={styles.logoContainer}>
+          <Image source={require('../assets/trailQuestLogoNoBG.png')} style={styles.logoImage} />
+        </View>      
+        <View style={styles.formContainer}>
+          <TextInput placeholder="Name" style={styles.input} autoCorrect={false} textContentType='oneTimeCode'/>
+          <TextInput placeholder="Username" style={styles.input} autoCorrect={false} textContentType='oneTimeCode'/>
+          <TextInput placeholder="password" secureTextEntry style={styles.input} autoCorrect={false} textContentType='oneTimeCode'/>
 
-        <AwesomeButton
-          type="primary"
-          onPress={() => navigation.navigate('Home')}
-          width={200} // Adjust as needed
-          height={50} // Adjust as needed
-          textSize={18} // Adjust as needed
-          backgroundColor="#D2DFAF"
-          backgroundDarker="#D2DFAF"
-          backgroundShadow="#D2DFAF"
-          textColor="#FFFFFF"
-          springRelease
-        >
-          Create My Account
-        </AwesomeButton>
-       
-      
+          <AwesomeButton
+            type="primary"
+            onPress={() => navigation.navigate('Home')}
+            width={200} // Adjust as needed
+            height={50} // Adjust as needed
+            textSize={18} // Adjust as needed
+            backgroundColor="#4CAF50"
+            backgroundDarker="#52a934"
+            backgroundActive="#7cbe2d"
+            backgroundShadow="#3f8228"
+            backgroundProgress="#89cf35"
+            borderColor="#5bbd3a"
+            textColor="#FFFFFF"
+            springRelease
+          >
+            Create My Account
+          </AwesomeButton>
+          <View style={styles.backToLogin}>
+              <TouchableOpacity onPress={() => navigation.navigate('SignIn')}>
+                <Text style={styles.navItem}>Back To Login</Text>
+              </TouchableOpacity>
+          </View>
+        </View>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -41,13 +61,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },  
   input: {
-    height: 40, // Adjust as needed
-    width: 300, // Adjust as needed
+    height: 40,
+    width: 300,
     borderColor: '#4CAF50',
+    backgroundColor: "white",
     borderWidth: 1,
     borderRadius: 10,
-    backgroundColor: "white",
-    padding: 20,
+    padding: 10,
     marginBottom: 30,
   },
   logoImage: {
@@ -64,5 +84,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     top: -10,
+  },
+  backToLogin: {
+    padding: 15,
+  },
+  navItem: {
+    textDecorationLine: 'underline',
+    fontFamily: 'RobotoSlab_600SemiBold',
   },
 });
